7645a3702fc67a035b4e254fed8a79929f1e2a84
Davide Termite <davide.termite@gmail.com>
2020-07-01 11:09:37 +0200

    Setup Milestone 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+
+})
+
+
+
+
+
+
+
+////////////////// API DATAS ////////////////////
+
+// api_key: 021f10396f4dfd536802346c3b13f5d1
+// endpoint: https://developers.themoviedb.org/3/search/movie
