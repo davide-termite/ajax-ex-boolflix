61fef3a95b75735fb63d6ead8bcc27aefa0376a6
Davide Termite <davide.termite@gmail.com>
2020-07-02 12:51:44 +0200

    Milestone 2 & Milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -10,54 +10,159 @@ $(document).ready(function(){
     // Se la searchbar non è vuota, richiamo funzione
     if (searchQuery != "") {
       searchMovie(searchQuery);
+      searchSerie(searchQuery);
       $(".results").text(" ");
+    }
+  });
+
+  // Al premere del tasto invio" vengono mostrati film che
+  // contengono la query inserita
+  $("#searchbar").keypress(function(){
+
+    // Richiamo il valore dell'input SEARCHBAR in una variabile
+    var searchQuery = $("#searchbar").val();
+
+    if (event.which == 13 || event.keycode == 13) {
+      // Se la searchbar non è vuota, richiamo funzione
+      if (searchQuery != "") {
+        searchMovie(searchQuery);
+        searchSerie(searchQuery);
+        $(".results").text(" ");
+      }
     }
   });
 });
 
+// funzione che resetta risultati ricerca precedente
+//   --> return: nulla
+function reset() {
+  $(".results").html("");
+}
 
 // funzione che permette di cercare film tramito l'utilizzo dell'API
-//   --> movieQuery: stringa, contiene parola o nome del film
+//   --> query: stringa, contiene parola o nome del film
 //   --> return: nulla
-function searchMovie(movieQuery) {
+function searchMovie(query) {
+  var urlMovies = "https://api.themoviedb.org/3/search/movie";
+  var api_key = "021f10396f4dfd536802346c3b13f5d1";
+
   $.ajax(
     {
-      url: "https://api.themoviedb.org/3/search/movie",
+      url: urlMovies,
       method: "GET",
       data: {
-        api_key:"021f10396f4dfd536802346c3b13f5d1",
-        query: movieQuery,
+        api_key: api_key,
+        query: query,
         language: "it-IT",
       },
+
       success: function(data){
         var queryResult = data.results;
-
-        for (var i = 0; i < queryResult.length; i++) {
-
-          var movie = {
-            title: queryResult[i].title,
-            // cover: ""queryResult[i].poster_path,
-            original_title: queryResult[i].original_title,
-            tagline: queryResult[i].tagline,
-            lang: queryResult[i].original_language,
-            rate: queryResult[i].vote_average,
-          }
-
-          displayMovie(movie)
-        }
+        populateMovie(queryResult);
 
       },
+
       error: function(errore){
         alert("C'è stato un errore " + errore);
-      }
+      },
     }
   )
 };
+function searchSerie(query) {
 
+  var urlSeries = "https://api.themoviedb.org/3/search/tv";
+  var api_key = "021f10396f4dfd536802346c3b13f5d1";
+
+  $.ajax(
+    {
+      url: urlSeries,
+      method: "GET",
+      data: {
+        api_key: api_key,
+        query: query,
+        language: "it-IT",
+      },
+
+      success: function(data){
+        var queryResult = data.results;
+        populateSeries(queryResult);
+
+      },
+
+      error: function(errore){
+        alert("C'è stato un errore " + errore);
+      },
+    }
+  )
+}
+
+// funzione che permette di cercare film tramito l'utilizzo dell'API
+//   --> queryResult: array, contiene oggetti, risultati della ricerca
+//   --> return: nulla
+function populateMovie(queryResult) {
+  var imgServer = "https://image.tmdb.org/t/p/w300";
+
+  for (var i = 0; i < queryResult.length; i++) {
+
+    var movie = {
+      title: queryResult[i].title,
+      cover: imgServer + queryResult[i].poster_path,
+      original_title: queryResult[i].original_title,
+      tagline: queryResult[i].tagline,
+      lang: queryResult[i].original_language,
+      rate: ratingStar(queryResult[i].vote_average),
+    }
+
+    displayResult(movie);
+  }
+
+}
+function populateSeries(queryResult) {
+  var imgServer = "https://image.tmdb.org/t/p/w300";
+
+  for (var i = 0; i < queryResult.length; i++) {
+
+    var serie = {
+      title: queryResult[i].name,
+      cover: imgServer + queryResult[i].poster_path,
+      original_title: queryResult[i].original_name,
+      tagline: queryResult[i].tagline,
+      lang: queryResult[i].original_language,
+      rate: ratingStar(queryResult[i].vote_average),
+    }
+
+    displayResult(serie);
+  }
+
+}
+
+// funzione che converte rating da un max di 10 a un max di 5
+//   --> number: voto del film
+//   --> return: stars >> array mostra voto in stelle
+function ratingStar(number){
+  if (number !== 0 || number !== ""){
+
+    var rating = Math.ceil(number/2);
+    var stars = [];
+    var i = 0;
+
+    while (i < 5) {
+      if(i <= rating) {
+        stars += '<i class="fas fa-star"></i>';
+        i++
+      } else {
+        stars += '<i class="far fa-star"></i>';
+        i++
+      }
+    }
+  }
+
+  return stars;
+}
 // funzione che stampa film con l'utilizzo di Handlebars
 //   --> movie: oggetto, contiene dati con cui popolare il template
 //   --> return: nulla
-function displayMovie(movie){
+function displayResult(movie){
   var source = $("#movie-template").html();
   var template = Handlebars.compile(source);
 
@@ -65,7 +170,11 @@ function displayMovie(movie){
   $(".results").append(html);
 }
 
+
+/////////////////////////////////////////////////
 ////////////////// API DATAS ////////////////////
+/////////////////////////////////////////////////
 
 // api_key: 021f10396f4dfd536802346c3b13f5d1
-// endpoint: https://developers.themoviedb.org/3/search/movie
+// endpoint movie: https://developers.themoviedb.org/3/search/movie
+// endpoint serie: https://developers.themoviedb.org/3/search/tv
